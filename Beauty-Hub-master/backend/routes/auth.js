2ebbd3ae97f25bb54837bbbd99fd16a16ef8591c
Sarah <shandriasarahdev@gmail.com>
2026-05-30 20:57:57 -0300

    test

--- Beauty-Hub-master/backend/routes/auth.js
+++ Beauty-Hub-master/backend/routes/auth.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+// Permite CORS para todas as rotas deste router
+router.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
+
+router.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.sendStatus(200);
+});
 
 // Login Cliente
 router.post("/login-cliente", async (req, res) => {
